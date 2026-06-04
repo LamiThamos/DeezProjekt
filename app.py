2e850ae712aed773b5831154cfd6df90baab9811
LamiThamos <creepboten@gmail.com>
2026-06-04 19:07:17 +0200

    Updated start page text and now sends argument to the search page to display all professors from the start

--- app.py
+++ app.py
@@ -15,10 +15,13 @@ app.register_blueprint(courseList.bp)
 def hello_world():
     return """
     <h1>Welcome to our DIS Project!</h1>
-    <p>Our project is about searching for...</p>
+    <p>For our project, we have created a simple web application to search for KU professors and find the courses that they teach.
+    For each professor, we display their weighted overall grade average and pass percentage.</p>
+    <p>It is just a proof of concept, since the database consists of just a sample of the data from the official KU website.
+    Ideally, we would scrape the KU website to populate the database with much more data, but we did not quite have enough time to implement a scraper.</p>
     <p>Click the button below to go to the professor search page.</p>
 
-    <a href="/professorSearch">
+    <a href="/professorSearch?professor_name=" class="nav-button">
         <button>Search for KU professors</button>
     </a>
     """
