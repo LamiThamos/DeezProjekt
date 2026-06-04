7c1573d4f0b2f6bc01995c9e0054cf7347acf06d
LamiThamos <creepboten@gmail.com>
2026-06-04 19:12:33 +0200

    Removed commented code

--- models/Courses.py
+++ models/Courses.py
@@ -37,23 +37,3 @@ def list_courses(professor):
      return professor_courses
      
 
-
-# def list_categories():
-#     conn = db_connection()
-#     cur = conn.cursor()
-#     cur.execute('SELECT id, category_name FROM categories')
-#     db_categories = cur.fetchall()
-
-#     categories = []
-#     for db_category in db_categories:
-#         categories.append(Category(db_category[0], db_category[1]))
-#     conn.close()
-#     return categories
-
-# def insert_category(category_name):
-#     conn = db_connection()
-#     cur = conn.cursor()
-#     cur.execute('INSERT INTO categories (category_name) VALUES (%s) ON CONFLICT DO NOTHING', (category_name,))
-#     conn.commit()
-#     cur.close()
-#     conn.close()
